32215d461c2fd9740c2757dcfd18031baf637f0c
alexbotha <alexbotha@hotmail.co.uk>
2023-08-14 09:50:26 +0100

    will install react bootstrap from npm

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "./index.css";
 import App from "./components/App";
+
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
