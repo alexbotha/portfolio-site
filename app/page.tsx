340394a535900dde1897b74931f887a5d0cf6aa2
alex botha <alex.botha@callenlenz.com>
2024-03-19 21:13:42 +0000

    update to styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,54 @@
+"use client";
+import alex from "@/public/alex2.jpg";
+import Image from "next/image";
+import { useState } from "react";
+import {
+  IconArrowNarrowRight,
+  IconDownload,
+  IconMenu2,
+} from "@tabler/icons-react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [openResume, setOpenResume] = useState(false);
+
+  return (
+    <div className="fixed text-label flex items-center gap-8 justify-center pt-20">
+      <div className="mr-14">
+        <Image
+          className="border rounded-full w-[auto] h-[auto] box-effect "
+          src={alex}
+          alt="profile photo"
+        />
+      </div>
+
+      <div className="text-[25px]">
+        <h1>Hi! I'm Alex.</h1>
+        <div className="text-auto">
+          <p>
+            A Full-Stack Developer passionate about creating software solutions
+            to help businesses
+          </p>
+        </div>
+        <div className="gap-2 py-2 items-center text-base">
+          <button
+            onClick={() => setOpenResume(!openResume)}
+            className="flex bg-white text-black hover:bg-blue-600 hover:text-white  rounded-[40px] gap-2 p-4 font-bold transition-colors duration-300"
+          >
+            Resume
+            <IconMenu2 />
+          </button>
+          <div
+            className={`${openResume ? "cursor-pointer" : "invisible"} pt-2`}
+          >
+            <div className="flex gap-2 hover:underline ">
+              <IconArrowNarrowRight /> Open
+            </div>
+            <div className="flex gap-2 hover:underline">
+              <IconDownload /> Download
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,13 @@
-import {
-  IconBrandLinkedin,
-  IconBrandGithub,
-  IconBrandMedium,
-} from "@tabler/icons-react";
+import { FaLinkedinIn, FaMediumM } from "react-icons/fa";
+import { TbBrandGithubFilled } from "react-icons/tb";
 
 import Link from "next/link";
 
 const githubURL = "https://github.com/alexbotha";
 const linkedinURL = "https://www.linkedin.com/in/alexbotha1996/";
 const mediumURL = "https://medium.com/@alexbotha_18115";
+
+const hoverState = "transition-transform transform-gpu hover:scale-110 p-1 ";
 
 export default function Navbar() {
   return (
@@ -19,21 +18,39 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className="flex nav-styles gap-7">
-        <Link href="/projects">PROJECTS</Link>
-        <Link href="/about">ABOUT</Link>
-        <Link href="/contact">CONTACT</Link>
+      <div className="flex nav-styles sm:gap-6 gap-2">
+        <Link className="hover:underline" href="/projects">
+          PROJECTS
+        </Link>
+        <Link className="hover:underline" href="/about">
+          ABOUT
+        </Link>
+        <Link className="hover:underline" href="/contact">
+          CONTACT
+        </Link>
       </div>
 
-      <div className="ml-6 flex gap-2.5">
-        <a href={githubURL} target="_blank">
-          <IconBrandGithub />
+      <div className="ml-8 flex sm:gap-2.5 gap-2">
+        <a
+          className={`rounded-full bg-blue-600 ${hoverState}`}
+          href={githubURL}
+          target="_blank"
+        >
+          <TbBrandGithubFilled />
         </a>
-        <a href={linkedinURL} target="_blank">
-          <IconBrandLinkedin />
+        <a
+          className={`rounded bg-blue-600 ${hoverState}`}
+          href={linkedinURL}
+          target="_blank"
+        >
+          <FaLinkedinIn />
         </a>
-        <a href={mediumURL} target="_blank">
-          <IconBrandMedium />
+        <a
+          className={`rounded-full bg-blue-600 ${hoverState}`}
+          href={mediumURL}
+          target="_blank"
+        >
+          <FaMediumM />
         </a>
       </div>
     </div>
